2e7d21e3b3c7fccf54cd46fc45d4b6758dfcfbd7
steeve mellet <melletsteeve@orange.fr>
2023-02-16 18:18:34 +0100

    create member area

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,9 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Form\ArticleType;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class ArticleController extends AbstractController
 {
@@ -15,4 +21,34 @@ class ArticleController extends AbstractController
             'controller_name' => 'ArticleController',
         ]);
     }
+
+    #[Route('/add/article', name: 'app_add_article')]
+    public function addArticle(EntityManagerInterface $entityManager, Request $request, UserInterface $user, UserRepository $userRepository)
+    {
+
+        $currentUser = $userRepository->findBy(['username'=>$user->getUserIdentifier()])[0];
+        $article = new Article();
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $date = new \DateTime();
+            $article->setDate($date);
+            $article->setUser($currentUser);
+
+            $entityManager->persist($article);
+            $entityManager->flush();
+
+        }
+
+
+        return $this->render('article/addArticle.html.twig', [
+            'form'=>$form->createView(),
+        ]);
+
+    }
+
+
+
 }
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +12,8 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager, UserRepository $userRepository): Response
     {
-
-
 
         return $this->render('home/index.html.twig');
     }

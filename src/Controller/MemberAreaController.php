dbe2e447cb843acd87cd015d98512526b3546150
steeve mellet <melletsteeve@orange.fr>
2023-02-16 18:18:34 +0100

    create member area

--- src/Controller/MemberAreaController.php
+++ src/Controller/MemberAreaController.php
@@ -2,17 +2,27 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class MemberAreaController extends AbstractController
 {
     #[Route('/member/area', name: 'app_member_area')]
-    public function index(): Response
+    public function index(UserInterface $user, UserRepository $userRepository): Response
     {
-        return $this->render('member_area/index.html.twig', [
-            'controller_name' => 'MemberAreaController',
-        ]);
+        if ($user){
+
+            $currentUser = $userRepository->findBy(['username'=>$user->getUserIdentifier()])[0];
+
+
+            return $this->render('member_area/index.html.twig', [
+                'user' => $currentUser,
+            ]);
+        } else {
+            return $this->render('security/index.html.twig');
+        }
     }
 }

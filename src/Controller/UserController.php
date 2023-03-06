48894fffa8f6bc180b40896069c78d4615929522
steeve mellet <melletsteeve@orange.fr>
2023-03-06 15:09:49 +0100

    user creation

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegistrationFormType;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
@@ -14,10 +19,35 @@ class UserController extends AbstractController
     {
         $users = $userRepository->findAll();
 
-
-
         return $this->render('user/index.html.twig', [
             'users' => $users,
         ]);
     }
+
+    #[Route('/user/add', name: 'app_user_add')]
+    public function addUser(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $userPasswordHasher){
+
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+
+            $user->setPassword(
+                $userPasswordHasher->hashPassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+            $entityManager->persist($user);
+            $entityManager->flush();
+        }
+
+        return $this->render('user/user_add.html.twig', [
+            'form'=>$form->createView()
+        ]);
+    }
+
+
+
 }
